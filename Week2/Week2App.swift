c1b0aa6e5d46ba1684d4c3019ce330b1162cd71b
Adam Walka <walka.adam@proton.me>
2023-12-01 19:42:30 +0100

    Initial Commit

---
+++ Week2/Week2App.swift
@@ -0,0 +1,17 @@
+//
+//  Week2App.swift
+//  Week2
+//
+//  Created by user249775 on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
