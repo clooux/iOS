dc5d321b1ca1593dd118c26eb9d5e2cc492a2a5e
Adam Walka <walka.adam@proton.me>
2023-12-06 20:41:04 +0100

    Week2 full app

--- Week2/ContentView.swift
+++ Week2/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tasks: [Task] = [
+        Task(id:1, name: "Task1", completed: false, image: "potato"),
+        Task(id:2, name: "Task2", completed: false),
+        Task(id:3, name: "Task3", completed: false),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List($tasks) { $task in
+                HStack{
+                    Button(action: {
+                        if let index = tasks.firstIndex(where: {$0.id == task.id}){
+                            tasks[index].completed = !tasks[index].completed
+                            print(tasks[index].completed)
+                        }
+                    }) {
+                        Image(systemName: task.completed ? "checkmark.circle" : "circle").foregroundColor(task.completed ? .green : .gray)
+                    }
+                    HStack{
+                        NavigationLink(destination: TaskView(task: $task)){
+                            Text(task.name)
+                        }
+                    }
+                }
+                .swipeActions(edge: .leading){
+                    Button(role: .destructive, action: {
+                        if let index = tasks.firstIndex(where: {$0.id == task.id}){
+                            tasks.remove(at: index)
+                        }
+                    })
+                    {
+                        Label("Delete", systemImage: "trash")
+                    }
+                }
+            }
+            
+            .padding()
+            .navigationTitle("Task List")
         }
-        .padding()
     }
 }
 
