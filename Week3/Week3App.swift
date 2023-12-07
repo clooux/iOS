9ee1ad32f1f024c55ec1b9e0499831d22fe136b0
Adam Walka <walka.adam@proton.me>
2023-12-07 12:56:41 +0100

    Initial Commit

---
+++ Week3/Week3App.swift
@@ -0,0 +1,20 @@
+//
+//  Week3App.swift
+//  Week3
+//
+//  Created by user249775 on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Week3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
