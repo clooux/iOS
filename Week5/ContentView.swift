66ea01ba289a08800739749f38b346af6b5053e0
Adam Walka <walka.adam@proton.me>
2024-01-07 18:55:43 +0100

    Initial Commit

---
+++ Week5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week5
+//
+//  Created by user249775 on 1/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week5/Week5App.swift
@@ -0,0 +1,17 @@
+//
+//  Week5App.swift
+//  Week5
+//
+//  Created by user249775 on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Week5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
